8870842ed56406498b15229322f51e6d8e2892a9
Nandovbb <49496507+Nandovbb@users.noreply.github.com>
2026-09-02 21:00:15 -0300

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -1,2 +1,17 @@
 # Receba os valores de 2 catetos de um triângulo retângulo. Calcule e mostre a hipotenusa
 # declarar
+a: float = 0
+b: float = 0
+h: int = 0
+# inicio
+# recebe o valor de a
+a = float(input('Digite o valor de A: '))
+
+# recebe o valor de b
+b = float(input('Digite o valor de B: '))
+
+# calcula a hipotenusa
+h = (a**2 + b**2) ** 0.5
+
+# retona a hipotenusa
+print('O valor da hipotenusa é:', int(h))
